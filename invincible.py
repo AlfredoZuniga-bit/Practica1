d6096a7288e286c191caa12220290c91620d1c18
Alfredo <alfredo.zunigar@uanl.edu.mx>
2026-04-20 08:43:08 -0600

    Corregí superpoderes por planeta de origen y añadí los setters

--- invincible.py
+++ invincible.py
@@ -1,8 +1,8 @@
 class Invincible:
-    def __init__(self, nombre, edad, superpoderes, velocidad, fuerza):
+    def __init__(self, nombre, edad, planeta, velocidad, fuerza):
         self.__nombre = nombre
         self.__edad = edad
-        self.__superpoderes = superpoderes
+        self.__planeta = planeta
         self.__velocidad = velocidad
         self.__fuerza = fuerza
 
@@ -13,11 +13,27 @@ class Invincible:
     def get_edad(self):
         return self.__edad
 
-    def get_superpoderes(self):
-        return self.__superpoderes
+    def get_planeta(self):
+        return self.__planeta
 
     def get_velocidad(self):
         return self.__velocidad
 
     def get_fuerza(self):
         return self.__fuerza
+
+    ##Setters
+    def set_nombre(self, nombre):
+        self.__nombre = nombre  
+
+    def set_edad(self, edad):
+        self.__edad = edad
+    
+    def set_planeta(self, planeta):
+        self.__planeta = planeta
+
+    def set_velocidad(self, velocidad):
+        self.__velocidad = velocidad    
+
+    def set_fuerza(self, fuerza):
+        self.__fuerza = fuerza
