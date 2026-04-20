7022cc5b81a680050ba262628c04d032ec075203
Alfredo <alfredo.zunigar@uanl.edu.mx>
2026-04-20 08:33:30 -0600

    se añadió la clase

---
+++ invincible.py
@@ -0,0 +1,9 @@
+class Invincible:
+    def __init__(self, nombre, edad, superpoderes, velocidad, fuerza):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__superpoderes = superpoderes
+        self.__velocidad = velocidad
+        self.__fuerza = fuerza
+
+        
